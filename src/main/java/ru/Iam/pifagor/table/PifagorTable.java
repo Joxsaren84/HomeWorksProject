1304171fb0bf7ed3dccfda84ac15df2df6b99ae1
Keypa <42-18@mail.ru>
2024-04-07 15:06:53 +1000

    add output the table with a printf method

--- src/main/java/ru/Iam/pifagor/table/PifagorTable.java
+++ src/main/java/ru/Iam/pifagor/table/PifagorTable.java
@@ -1,7 +1,13 @@
 package ru.Iam.pifagor.table;
 
 public class PifagorTable {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
+        for(int i = 1; i < 10; i++){
+            for(int j = 1; j < 10; j++){
+                System.out.printf("%3d", i * j);
+            }
+            System.out.print("\n");
+        }
     }
 }
