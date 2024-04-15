536628d930bc2a5059807fb65ad9ecff4dfb5603
Keypa <42-18@mail.ru>
2024-04-15 16:49:12 +1000

    add Lesson15

---
+++ src/main/java/ru/Iam/tregulov/Lesson15/Lesson15.java
@@ -0,0 +1,27 @@
+package ru.Iam.tregulov.Lesson15;
+
+public class Lesson15 {
+    public static void main(String[] args) {
+
+        int h = 0;
+        INNER:
+        while(h < 24){
+            int m = -1;
+            MIDDLE:
+            do{
+                m++;
+                if (h > 1 & m % 10 == 0)
+                    break INNER;
+                int s = 0;
+                OUTER:
+                while (s < 60){
+                    if (s * h > m)
+                        continue MIDDLE;
+                    System.out.println(h + ":" + m + ":" + s);
+                    s++;
+                }
+            }while(m < 59);
+            h++;
+        }
+    }
+}
