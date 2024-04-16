c9217bdf96fc9fdb03f0b598e440f5465ad38162
Keypa <42-18@mail.ru>
2024-04-16 15:06:57 +1000

    add Lesson16

---
+++ src/main/java/ru/Iam/tregulov/lesson16/Lesson16.java
@@ -0,0 +1,17 @@
+package ru.Iam.tregulov.lesson16;
+
+public class Lesson16 {
+
+    static void email(String str){
+
+        while (str.length() > 0) {
+            System.out.println(str.substring(str.indexOf('@') + 1, str.indexOf('.'))); //находим и выводим первую подстроку от @ до .
+            str = str.substring(str.indexOf(';') + 1); //обрезаем к чертям все что до первого вхождения ';' и саму запеточку
+        }
+    }
+
+    public static void main(String[] args) {
+        String emailsList = "ya@yahoo.com; on@mail.ru; ona@gmail.com; hhh@yandex.ru; bb@sinara.ru;";
+        email(emailsList);
+    }
+}
