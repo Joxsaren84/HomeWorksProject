b175bfa481804fe7ee609c167b094a5d4c6851f4
Keypa <42-18@mail.ru>
2024-04-17 14:14:09 +1000

    add Lesson17

---
+++ src/main/java/ru/Iam/tregulov/lesson17/Lesson17.java
@@ -0,0 +1,25 @@
+package ru.Iam.tregulov.lesson17;
+
+public class Lesson17 {
+
+    static boolean ravenstvo(StringBuilder sb1, StringBuilder sb2){
+        if (sb1 == sb2){
+            return true;
+        }else{
+            String s1 = new String(sb1);
+            String s2 = new String(sb2);
+            return s1.equals(s2);
+        }
+    }
+
+    public static void main(String[] args) {
+        StringBuilder stb1 = new StringBuilder("Hello world");
+        StringBuilder stb2 = stb1;
+        StringBuilder stb3 = new StringBuilder("Hello Lord");
+        StringBuilder stb4 = new StringBuilder("Hello world");
+
+        System.out.println(ravenstvo(stb1, stb2));
+        System.out.println(ravenstvo(stb1, stb3));
+        System.out.println(ravenstvo(stb1, stb4));
+    }
+}
